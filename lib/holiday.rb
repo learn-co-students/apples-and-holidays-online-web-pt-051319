0bd275b12bd4d399be24faa163a795c52ac8cdda
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-05-26 19:12:44 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,11 +2,17 @@ require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
+  def second_supply_for_fourth_of_july(holiday_supplies)
+    
+    holiday_supplies [:summer] [:fourth_of_july][1]
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
   #     :new_years => ["Party Hats"]
-  #   },
+  #   },winter_supplies(holiday_supplies)
+  3
+  
+  
   #   :summer => {
   #     :fourth_of_july => ["Fireworks", "BBQ"]
   #   },
